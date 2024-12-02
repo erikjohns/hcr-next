1fe2188e82dfc823ba76bf695d1f618bb782cceb
Johns, Erik Michael <erikj3520@gmail.com>
2024-12-02 16:04:04 -0700

    Added Text to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,14 @@ import '@/styles/Footer.css';
 export default function Footer() {
     return (
         <footer>
-            <LogoDark />
+            <div>
+                <LogoDark className={'footer-logo'}/>
+                <div className={'footer-text'}>
+                    <h1>Hail Columbia Ranch</h1>
+                    <p>&copy;2024 Hail Columbia Ranch. All Rights Reserved</p>
+                </div>
+            </div>
+
             <nav>
                 <Link href={'/homepage'} className={'footer-link'}><span className="main-navbar-hover-underline footer-navbar-hover-underline">Home</span></Link>
                 <Link href={'/homepage'} className={'footer-link'}><span className="main-navbar-hover-underline footer-navbar-hover-underline">Venue</span></Link>
