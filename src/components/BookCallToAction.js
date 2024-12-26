94a0f0153e0aa049ecf449564d7ef59f48ba0760
Johns, Erik Michael <erikj3520@gmail.com>
2024-12-26 16:15:21 -0700

    Added Call to Action

---
+++ src/components/BookCallToAction.js
@@ -0,0 +1,7 @@
+export default function BookCallToAction() {
+    return (
+        <div className="bookCallToAction">
+            <h4>Interested in our spaces? Contact Us!</h4>
+        </div>
+    )
+}
