c8cbc9d97ed2d7752fd6afc4745d6f20187ef874
Johns, Erik Michael <erikj3520@gmail.com>
2024-12-26 16:14:32 -0700

    Adjusted Homepage Section

--- src/components/HomepageSection.js
+++ src/components/HomepageSection.js
@@ -8,7 +8,7 @@ export default function HomepageSection({ data }) {
             <div className={data.left ? 'homepage-section-text-container' : 'homepage-section-text-container homepage-section-text-right-align'}>
                 <h1>{data.title}</h1>
                 <h4>{data.subtitle}</h4>
-                <p>{data.text}</p>
+                <p dangerouslySetInnerHTML={{ __html: data.text }}></p>
             </div>
             <img src={data.image} alt={data.title} className={data.left ? 'homepage-section-image' : 'homepage-section-image homepage-section-image-left-align'}/>
         </div>
